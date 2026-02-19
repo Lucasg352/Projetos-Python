6466ffcba6503f2214c97e8d3232474f8694a2c1
Lucasg352 <lucassgabriel346@gmail.com>
2026-02-19 02:59:13 -0300

    Comentários da lógica usada

--- logica_python/exercicio_par_impar.py
+++ logica_python/exercicio_par_impar.py
@@ -6,15 +6,13 @@ inteiro, informe que não é um número inteiro.
 
 numero_1=input('Digite um número inteiro:')
 
-try:
-    numero_int = int(numero_1)
+try: #Para "capturar" um possível futuro erro quando não for inserido um número inteiro
+    numero_int = int(numero_1) # Uma variável feita para converter o retorno do input para int
 
-    if numero_int % 2 == 0:
+    if numero_int % 2 == 0: #Checagem do resto da divisão
         print(f'O número {numero_1} é PAR')
-    elif numero_int % 2 == 1:
-        print(f'O número {numero_1} é IMPAR')
     else:
-        print('Número não inteiro')
+        print('Número é IMPAR')
 
-except:
+except: #Irá exibir a mensagem abaixo quando um número não inteiro for inserido que ocasionaria um erro
     print('Não é um número inteiro')
